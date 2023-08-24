2603899531b96222fcd4bcef0eb7fbef3054c451
Petr Pavliuchenko <pavliuchenko.p@gmail.com>
2023-08-25 01:26:35 +0300

    Add odds function

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+from datetime import datetime
+import random
+from time import sleep
 
-print("Hello")
+odds = [1,  3,  5,  7,  9, 11, 13, 15, 17, 19,
+       21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
+       41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
 
-print("End")
+for i in range(5):
+    right_this_minute = datetime.today().minute
 
-print("switch to develop")
+    if right_this_minute in odds:
+        print("This minute seems a little odd.")
+    else:
+        print("Not an odd minute.")
+    wait_time = random.randint(1, 60)
+    sleep(wait_time)
+
+
